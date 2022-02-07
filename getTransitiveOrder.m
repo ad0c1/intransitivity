fd7e447446626eba85d03f2dde3145e2330fe806
ad0c1 <shaman_korea@mac.com>
2022-02-08 02:21:05 +0900

    Update getTransitiveOrder.m

--- getTransitiveOrder.m
+++ getTransitiveOrder.m
@@ -1,7 +1,6 @@
 function TransitiveOrder = getTransitiveOrder(A,excluded_nodes)
 label_array=1:length(A);
 label_array(excluded_nodes)=[];
-%%%%%%%
 A(excluded_nodes,:)=[];
 A(:,excluded_nodes)=[];
 [all_optimalOrders]=findOptimalOrders(A);
