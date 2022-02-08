f6ed8b206a68210922dffd6e607aa440cde23f12
ad0c1 <shaman_korea@mac.com>
2022-02-08 13:32:45 +0900

    Update makeAdjFromAvgMat.m

--- makeAdjFromAvgMat.m
+++ makeAdjFromAvgMat.m
@@ -1,5 +1,5 @@
 function B = makeAdjFromAvgMat(A)
-% input should be the adjacency matrix of the population ratio who made the choice for i over j in M(i,l)    
+% input should be the adjacency matrix of the population ratio who made the choice for i over j in M(i,j)    
 if find(A==0.5)
    error('Error. majority rule needs average other than 0.5');
 end
